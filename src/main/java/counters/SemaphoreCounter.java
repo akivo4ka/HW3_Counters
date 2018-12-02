d7fb9c3d588ac0e03880f8cc71dae2ec9a33261b
akivo4ka <akivavika@mail.ru>
2018-12-02 17:37:42 +0300

    HW3 Shared Counters

--- src/main/java/counters/SemaphoreCounter.java
+++ src/main/java/counters/SemaphoreCounter.java
@@ -9,7 +9,7 @@ public class SemaphoreCounter {
     private int counter = 0;
 
     public int incrementAndGet() throws InterruptedException {
-        int temp = 0;
+        int temp;
         try {
             semaphore.acquire();
             temp = ++counter;
@@ -18,16 +18,4 @@ public class SemaphoreCounter {
         }
         return temp;
     }
-
-//    public int incrementAndGet() throws InterruptedException {
-//        int temp = 0;
-//        try {
-//            semaphore.acquire();
-//            temp = counter++;
-//            System.out.println(temp);
-//        } finally {
-//            semaphore.release();
-//        }
-//        return temp;
-//    }
 }
